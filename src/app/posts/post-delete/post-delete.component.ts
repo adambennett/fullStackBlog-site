d951b40390d7ef016b73e59a1e1541817f42803f
Greg Davis <jrobley3@hotmail.com>
2020-04-16 15:17:59 -0400

    progress on edit and delete buttons

--- src/app/posts/post-delete/post-delete.component.ts
+++ src/app/posts/post-delete/post-delete.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GiphyService } from '../../services/giphy.service';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../../services/post/post.service';
 
 @Component({
   selector: 'app-post-delete',
   templateUrl: './post-delete.component.html',
   styleUrls: ['./post-delete.component.scss']
 })
-export class PostDeleteComponent implements OnInit {
+export class PostDeleteComponent implements OnDestroy, OnInit {
 
-  constructor() { }
+  post: any = {};
 
-  ngOnInit(): void {
+  sub: Subscription;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private postService: PostService,
+              private giphyService: GiphyService) { }
+
+  ngOnInit() {this.sub = this.route.params.subscribe(params => {
+    const id = params.id;
+    if (id) {
+      this.postService.get(id).subscribe((post: any) => {
+        if (post) {
+          this.post = post;
+          // this.post.href = post._links.self.href;
+          // this.giphyService.get(post.name).subscribe(url => post.giphyUrl = url);
+        } else {
+          console.log(`Post with id '${id}' not found, returning to list`);
+          this.gotoList();
+        }
+      });
+    }
+  });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  gotoList() {
+    this.router.navigate(['/post-list']);
+  }
+
+  // save(form: NgForm) {
+  //   this.postService.remove(this.post).subscribe(result => {
+  //     this.gotoList();
+  //   }, error => console.error(error));
+  // }
+
+  remove() {
+    this.postService.remove(this.post);
+    this.gotoList();
   }
 
 }
